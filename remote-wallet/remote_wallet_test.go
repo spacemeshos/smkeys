8009c05e61ded8e9112a19b0346863862166fc6f
Lane Rettig <lanerettig@gmail.com>
2023-05-08 13:52:09 -0400

    Skip ledger test in CI env

--- remote-wallet/remote_wallet_test.go
+++ remote-wallet/remote_wallet_test.go
@@ -2,10 +2,21 @@ package remote_wallet
 
 import (
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
+func skipCI(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping test in CI environment")
+	}
+}
+
 func TestReadPubkeyFromLedger(t *testing.T) {
+	// skip test in CI environment, it won't work as it requires a physical ledger device
+	// to be connected
+	skipCI(t)
+
 	derivationPath := "m/44'/540'/0'/0'/0'"
 	path := "usb://ledger"
 	key, err := ReadPubkeyFromLedger(path, derivationPath, true)
